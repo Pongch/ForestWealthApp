55f495bb2019a2b6be413138e8f233321f9ab26b
YOUR NAME <pong_thailand@hotmail.com>
2017-11-15 08:46:06 +0100

    improved interface

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ window.onload = function() {
       ForestWealth1.getCarbonEmissionByYear(carbonYear, function(error, result){
        if(!error)
             //add result to output-field
-            document.getElementById('output-field').innerHTML = result.slice(-1)[0]  ;
+            document.getElementById('output-field').innerHTML = "1st Year: " + result.slice(-1)[0] + " mTg C per year"  ;
        else
            console.error(error);}
 
@@ -41,7 +41,7 @@ window.onload = function() {
      ForestWealth1.getCarbonEmissionByYear(carbonYear2, function(error, result){
       if(!error)
            //add result to output-field
-           document.getElementById('output-field2').innerHTML = result.slice(-1)[0]  ;
+           document.getElementById('output-field2').innerHTML = "2nd Year: " + result.slice(-1)[0] + ' mTg C per year' ;
       else
           console.error(error);}
     );
@@ -56,7 +56,7 @@ window.onload = function() {
           ForestWealth1.getDeforestationByYear(forestYear1, function(error, result){
            if(!error)
                 //add result to output-field
-                document.getElementById('forest-output-field').innerHTML = result.slice(-1)[0]  ;
+                document.getElementById('forest-output-field').innerHTML = "1st Year: " + result.slice(-1)[0] + " ha per year" ;
            else
                console.error(error);}
 
@@ -66,13 +66,49 @@ window.onload = function() {
          ForestWealth1.getDeforestationByYear(forestYear2, function(error, result){
           if(!error)
                //add result to output-field
-               document.getElementById('forest-output-field2').innerHTML = result.slice(-1)[0]  ;
+               document.getElementById('forest-output-field2').innerHTML = "2nd Year: " + result.slice(-1)[0] + " ha per year" ;
           else
               console.error(error);}
         );
     }
 
-
+    var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 
 }
 //Chart.js
