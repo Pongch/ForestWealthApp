653d13819d509ced12c706431cba3d02823cc23e
YOUR NAME <pong_thailand@hotmail.com>
2017-11-15 05:29:26 +0100

    link smart contract to dapp

--- app.js
+++ app.js
@@ -11,15 +11,16 @@ if (typeof web3 !== 'undefined') {
 
 
 //call contract's ABI
-var ForestWealthDb =
-web3.eth.contract([{"constant":true,"inputs":[],"name":"count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"year","type":"uint256"}],"name":"getCarbonCreditByYear","outputs":[{"name":"getyear","type":"uint256"},{"name":"carboncredit","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"i","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"carboncredit","type":"uint256"},{"name":"year","type":"uint256"}],"name":"setRecord","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"index","type":"uint256"}],"name":"getCarbonCredit","outputs":[{"name":"year","type":"uint256"},{"name":"carboncredit","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
-)
+var ForestWealthDb1 =
+web3.eth.contract([{"constant":true,"inputs":[{"name":"year","type":"uint256"}],"name":"getCarbonEmissionByYear","outputs":[{"name":"getyear","type":"uint256"},{"name":"carbonEmission","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"year","type":"uint256"}],"name":"getDeforestationByYear","outputs":[{"name":"getyear","type":"uint256"},{"name":"deforestation","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"carbonEmission","type":"int256"},{"name":"year","type":"uint256"},{"name":"deforestation","type":"int256"}],"name":"setRecord","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"i","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"index","type":"uint256"}],"name":"getCarbonInfo","outputs":[{"name":"year","type":"uint256"},{"name":"carbonEmission","type":"int256"},{"name":"deforestation","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"}])
 
 
 //call Contract's Address- you must set this to a different address if using testRPC
-var ForestWealth = ForestWealthDb.at('0xcf2f788c1acebd6bf6b7e75a9fce23955c1120d6');
+var ForestWealth1 = ForestWealthDb1.at('0x9c185ed8948d1306211776dc520128c5ebeb5cdc');
 
 window.onload = function() {
+
+    //CARBON output
     // onclick will call outputCert
     document.getElementById("form-click").onclick = function(){outputCarbon()};
 
@@ -27,7 +28,7 @@ window.onload = function() {
       //chart js
 
       let carbonYear = document.getElementById("record-index").value;
-      ForestWealth.getCarbonCreditByYear(carbonYear, function(error, result){
+      ForestWealth1.getCarbonEmissionByYear(carbonYear, function(error, result){
        if(!error)
             //add result to output-field
             document.getElementById('output-field').innerHTML = result.slice(-1)[0]  ;
@@ -37,7 +38,7 @@ window.onload = function() {
      );
      //getting second value
      let carbonYear2 = document.getElementById("record-index2").value;
-     ForestWealth.getCarbonCreditByYear(carbonYear2, function(error, result){
+     ForestWealth1.getCarbonEmissionByYear(carbonYear2, function(error, result){
       if(!error)
            //add result to output-field
            document.getElementById('output-field2').innerHTML = result.slice(-1)[0]  ;
@@ -45,6 +46,25 @@ window.onload = function() {
           console.error(error);}
     );
 
+    //DEFORESTATION output
+    let forestYear1 = document.getElementById("forest-record-index").value;
+    ForestWealth1.getDeforestationByYear(forestYear1, function(error, result){
+     if(!error)
+          //add result to output-field
+          document.getElementById('forest-output-field').innerHTML = result.slice(-1)[0]  ;
+     else
+         console.error(error);}
+
+   );
+   //getting second value
+   let forestYear2 = document.getElementById("forest-record-index2").value;
+   ForestWealth1.getDeforestationByYear(forestYear2, function(error, result){
+    if(!error)
+         //add result to output-field
+         document.getElementById('forest-output-field2').innerHTML = result.slice(-1)[0]  ;
+    else
+        console.error(error);}
+  );
 
     }
 
